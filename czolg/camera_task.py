1dcccb7c0a1d19b3f461b4f21c9943eb800fa3c2
Carbon225 <carbon225@protonmail.com>
2025-04-20 01:04:49 +0200

    camera

---
+++ czolg/camera_task.py
@@ -0,0 +1,58 @@
+import time
+import socket
+from picamera2 import Picamera2, MappedArray
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FileOutput
+import cv2
+import threading
+
+
+class CameraTask:
+    def __init__(self, remote_ip: str, remote_port: int, preview: bool):
+        self.remote_ip = remote_ip
+        self.remote_port = remote_port
+        self.preview = preview
+        self.picam2 = Picamera2()
+        self.picam2.post_callback = self.post_callback
+        video_config = self.picam2.create_video_configuration(
+            main={"format": "YUV420"},
+            buffer_count=2,
+            queue=False,
+        )
+        self.picam2.configure(video_config)
+        self.encoder = H264Encoder(bitrate=1_000_000)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.stream = None
+        self.thread = threading.Thread(target=self.task)
+
+    def start(self):
+        self.picam2.start(show_preview=self.preview)
+        if self.remote_ip:
+            self.sock.connect((self.remote_ip, self.remote_port))
+            self.stream = self.sock.makefile("wb")
+            self.picam2.start_recording(self.encoder, FileOutput(self.stream))
+        self.thread.start()
+
+    def join(self):
+        self.thread.join()
+
+    def post_callback(self, request):
+        timestamp = time.strftime("%Y-%m-%d %X")
+        colour = (0, 255, 0)
+        origin = (0, 30)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        scale = 1
+        thickness = 2
+        with MappedArray(request, "main") as m:
+            cv2.putText(m.array, timestamp, origin, font, scale, colour, thickness)
+
+    def task(self):
+        try:
+            while True:
+                image = self.picam2.capture_array()
+                # TODO: do stuff
+                time.sleep(1)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.picam2.stop_recording()
--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import argparse
 from pymavlink import mavutil
 
 from czolg.mavlink_task import MavlinkTask
+from czolg.camera_task import CameraTask
 
 
 def main():
     parser = argparse.ArgumentParser(description='MAVLink Tank Controller')
+    
     parser.add_argument('--tty', type=str, help='Enable TTY mode with the given path')
     parser.add_argument('--baudrate', type=int, help='Baudrate for TTY mode')
+    
     parser.add_argument('--udpin', action='store_true', help='Enable UDPIN mode')
     parser.add_argument('--port', type=int, default=14550, help='Port for UDPIN mode')
+
+    parser.add_argument('--camera-viewer-ip', type=str, help='IP address of the camera viewer')
+    parser.add_argument('--camera-viewer-port', type=int, default=5000, help='Port of the camera viewer')
+
+    parser.add_argument('--camera-preview', action='store_true', help='Enable camera preview')
+
     args = parser.parse_args()
 
     connection = None
@@ -31,16 +40,23 @@ def main():
     else:
         print("Error: --tty or --udpin is required")
         return
-    
+
     if not connection:
         print("Error: Invalid connection parameters")
         return
     
+    if not args.camera_viewer_ip:
+        print("Warning: --camera-viewer-ip is not set, camera stream will not be sent")
+
     mavlink_task = MavlinkTask(connection)
+    camera_task = CameraTask(args.camera_viewer_ip, args.camera_viewer_port, args.camera_preview)
+
     mavlink_task.start()
+    camera_task.start()
 
     try:
         mavlink_task.join()
+        camera_task.join()
     except KeyboardInterrupt:
         print("Exiting...")
 
