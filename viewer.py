3a56ff8e52b6425a2be2880d2fdb06ca43abdd42
Carbon225 <carbon225@protonmail.com>
2025-04-22 15:15:43 +0200

    update viewer

--- viewer.py
+++ viewer.py
@@ -7,12 +7,14 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", 5000))
 
+    cv2.namedWindow("Camera", cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO | cv2.WINDOW_GUI_NORMAL)
+
     while True:
         data, addr = sock.recvfrom(65507)
         jpeg_buf = np.frombuffer(data, dtype=np.uint8)
         image = cv2.imdecode(jpeg_buf, cv2.IMREAD_COLOR)
         try:
-            cv2.imshow("image", image)
+            cv2.imshow("Camera", image)
         except cv2.error:
             pass
         if cv2.waitKey(1) & 0xFF == 27:
